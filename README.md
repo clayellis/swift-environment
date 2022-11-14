fc19e261ff312d9f86f640e97e83a724eee0d653
Clay Ellis <clayellis@me.com>
2022-11-13 18:45:10 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-environment
+
+A description of this package.
---
+++ Sources/swift-environment/swift_environment.swift
@@ -0,0 +1,6 @@
+public struct swift_environment {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
